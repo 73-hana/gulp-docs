34521243a90f75b07dd331fa68534d8a63782de0
73-hana <hanaprgrm1997@gmail.com>
2023-01-23 23:58:13 +0900

    タスクの再構成についてのメモ

--- sample-project/gulpfile.js
+++ sample-project/gulpfile.js
@@ -3,3 +3,14 @@ function defaultTask(cb) {
 }
 
 exports.default = defaultTask;
+
+const { series } = require("gulp");
+
+function clean(cb) {
+  cb();
+}
+function build(cb) {
+  cb();
+}
+exports.build = build;
+exports.default = series(clean, build);
